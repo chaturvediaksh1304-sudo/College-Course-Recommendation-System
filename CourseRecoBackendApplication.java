ada0255c12eec6f7be50342c332de251c584ef3a
Chaturvedi_Aksh <chaturvedi.aksh1304@gmail.com>
2025-12-23 17:36:50 +0530

    Add files via upload

---
+++ CourseRecoBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.coursereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRecoBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRecoBackendApplication.class, args);
+    }
+}
